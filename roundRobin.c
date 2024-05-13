6456d5e695ac8f1e4d2e000a470ac8baec0936ba
dragg07 <draggerr39@gmail.com>
2024-05-14 00:16:13 +0530

    galti krdi tune

--- roundRobin.c
+++ roundRobin.c
@@ -1,76 +1,67 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void findWaitingTime(int process[], int n, int bt[], int wt[], int quantum) {
-    int remaining_burst_time[n];
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum) {
+    int rem_bt[n];
     for (int i = 0; i < n; i++)
-        remaining_burst_time[i] = bt[i];
+        rem_bt[i] = bt[i];
 
     int t = 0;
     while (1) {
         int done = 1;
         for (int i = 0; i < n; i++) {
-            if (remaining_burst_time[i] > 0) {
-                done = 0; 
+            if (rem_bt[i] > 0) {
+                done = 0;
 
-                if (remaining_burst_time[i] > quantum) {
+                if (rem_bt[i] > quantum) {
                     t += quantum;
-                    remaining_burst_time[i] -= quantum;
+
+                    rem_bt[i] -= quantum;
                 } else {
-                    t += remaining_burst_time[i];
+                    t = t + rem_bt[i];
+
                     wt[i] = t - bt[i];
-                    remaining_burst_time[i] = 0;
+                    rem_bt[i] = 0;
                 }
             }
         }
+
         if (done == 1)
             break;
     }
 }
 
-void findTurnAroundTime(int process[], int n, int bt[], int wt[], int tat[]) {
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
     for (int i = 0; i < n; i++)
         tat[i] = bt[i] + wt[i];
 }
 
-void findAverageTime(int process[], int n, int bt[], int quantum) {
-    int wt[n], tat[n];
-    int total_wt = 0, total_tat = 0;
+void findavgTime(int processes[], int n, int bt[], int quantum) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
 
-    findWaitingTime(process, n, bt, wt, quantum);
-    findTurnAroundTime(process, n, bt, wt, tat);
-    printf("process   Burst time   Waiting time   Turnaround time\n");
+    findWaitingTime(processes, n, bt, wt, quantum);
+
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    printf("PN\tBT\tWT\tTAT\n");
 
     for (int i = 0; i < n; i++) {
-        total_wt += wt[i];
-        total_tat += tat[i];
-        printf("   %d ", i + 1);
-        printf("         %d ", bt[i]);
-        printf("          %d ", wt[i]);
-        printf("            %d\n", tat[i]);
+        total_wt = total_wt + wt[i];
+        total_tat = total_tat + tat[i];
+        printf("%d\t%d\t%d\t%d\n", i + 1, bt[i], wt[i], tat[i]);
     }
 
-    printf("\nAverage waiting time = %.2f", (float)total_wt / (float)n);
-    printf("\nAverage turnaround time = %.2f\n", (float)total_tat / (float)n);
+    printf("Average waiting time = %.2f\n", (float)total_wt / (float)n);
+    printf("Average turn around time = %.2f\n", (float)total_tat / (float)n);
 }
 
+// Driver code
 int main() {
-    int n;
-    printf("Enter no. of process : ");
-    scanf("%d", &n);
-    int process[n];
+    int processes[] = {1, 2, 3};
+    int n = sizeof processes / sizeof processes[0];
 
-    for(int i=0; i<n; i++){
-        process[i] = i+1;
-    }
+    int burst_time[] = {10, 5, 8};
 
-    int burst_time[n];
-    printf("Enter burst time for the processes: \n");
-    for(int i=0; i<n; i++){
-        printf("P[%d] : ", i+1);
-        scanf("%d",&burst_time[i]);
-    }
     int quantum = 2;
-
-    findAverageTime(process, n, burst_time, quantum);
+    findavgTime(processes, n, burst_time, quantum);
     return 0;
 }
